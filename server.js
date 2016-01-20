617c44c23993ee73df3eee8d739953c6038e1c61
DataNick <DataNick@users.noreply.github.com>
2016-01-19 20:50:19 -0500

    Clarified code for self-learning by adding comments

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express'),
-    app     = express();
+    app     = express(); // for express, we create a new application
+
+
+// defining new routes for app
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/client/views/index.html');
+  res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.listen(3000, function() {
