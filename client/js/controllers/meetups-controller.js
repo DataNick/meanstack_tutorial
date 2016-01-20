c961cc004ff26d4ad4f7abf3590c7e09f4e2fdc9
DataNick <DataNick@users.noreply.github.com>
2016-01-20 17:01:17 -0500

    Fixed typo

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,13 +1,17 @@
-app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resouce) {
-  var Meetup = $resource('/api/meetups')
+app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resource) {
+  var Meetup = $resource('/api/meetups'); // Build Meetup model and use base url of restful service for our model
+
   $scope.meetups = [
     {name: "MEAN SF Developers"},
     {name: "Some other meetup"}
   ]
 
   $scope.createMeetup = function () {
-    $scope.meetups.push({name: $scope.meetupName});
-    $scope.meetupName = '';
+    var meetup = new Meetup();
+    meetup.name = $scope.meetupName;
+    meetup.$save();
+    // $scope.meetups.push({name: $scope.meetupName});
+    // $scope.meetupName = '';
   }
 }]);
 
