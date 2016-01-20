33730027a49239066a231a0c9a704a6ea21582a8
DataNick <DataNick@users.noreply.github.com>
2016-01-20 11:22:04 -0500

    Defined meetupsController on angular app module

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,3 +1,12 @@
-function meetupsController($scope) {
-  $scope.meetupsCount = 10;
-}
+app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resouce) {
+  $scope.meetups = [
+    {name: "MEAN SF Developers"},
+    {name: "Some other meetup"}
+  ]
+
+  $scope.createMeetup = function () {
+    $scope.meetups.push({name: $scope.meetupName});
+    $scope.meetupName = '';
+  }
+}])
+
