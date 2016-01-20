b005c40e7ff6a1c856fa60d158c39a28a3e1e118
DataNick <DataNick@users.noreply.github.com>
2016-01-20 18:13:36 -0500

    Required mongoose and connected to mongodb database

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
-var express = require('express'),
-    app     = express(); // for express, we create a new application
+//server needs to pass to controller
+var express           = require('express'),
+    app               = express(),   // for express, we create a new application
+    bodyParser        = require('body-parser'),
+    mongoose          = require('mongoose'),
+    meetupsController = require('./server/controllers/meetups-controller');
 
-
+mongoose.connect('mongodb://localhost:27017/mean-sample');
 // defining new routes for app
+
+app.use(bodyParser()); //middleware to parse body out
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/client/views/index.html');
 });
 
 app.use('/js', express.static(__dirname + '/client/js'));
+
+app.post('/api/meetups', meetupsController.create); // passed a request and response by express
+
+
 
 app.listen(3000, function() {
   console.log("I'm listening...")
