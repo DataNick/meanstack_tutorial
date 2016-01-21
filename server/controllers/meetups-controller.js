0b363e64e4b6e0540ace627861ccbe603e97e0e4
DataNick <DataNick@users.noreply.github.com>
2016-01-20 19:13:35 -0500

    List takes request and response and find from mongoose the meetup list and serve up the results

--- server/controllers/meetups-controller.js
+++ server/controllers/meetups-controller.js
@@ -6,5 +6,13 @@
 var Meetup = require('../models/meetup');
 module.exports.create = function (req, res) {  // passed a request and response by express
   var meetup = new Meetup(req.body);
-  meetup.save();
+  meetup.save(function (err, result) {
+    res.json(result); // send that result down to client with id and everything
+  });
 };
+
+module.exports.list = function (req, res) {
+  Meetup.find({}, function (err, results) {
+    res.json(results);
+  });
+}
