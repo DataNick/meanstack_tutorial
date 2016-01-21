a0bf3f28cb8c316010dfd90af9670c6ded700862
DataNick <DataNick@users.noreply.github.com>
2016-01-20 19:08:35 -0500

    Removed array of meetups; running query to find and list meetups from mongo database

--- client/js/controllers/meetups-controller.js
+++ client/js/controllers/meetups-controller.js
@@ -1,15 +1,19 @@
 app.controller('meetupsController', ['$scope', '$resource', function ($scope, $resource) {
   var Meetup = $resource('/api/meetups'); // Build Meetup model and use base url of restful service for our model
+  // Syntax find meetups; use a get request againt api/meetups
+  Meetup.query(function (results) {
+    $scope.meetups = results; // only work if create a get route for a restful service for meetups
+  });
 
-  $scope.meetups = [
-    {name: "MEAN SF Developers"},
-    {name: "Some other meetup"}
-  ]
+  $scope.meetups = []
 
   $scope.createMeetup = function () {
     var meetup = new Meetup();
     meetup.name = $scope.meetupName;
-    meetup.$save();
+    meetup.$save(function (result) { // callback function
+      $scope.meetups.push(result); // set new meetup onto the collection
+      $scope.meetupName = '';
+    });
     // $scope.meetups.push({name: $scope.meetupName});
     // $scope.meetupName = '';
   }
